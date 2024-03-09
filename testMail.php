8090116f88a85842cff186b1b5b302823200211d
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2024-03-09 16:21:53 -0500

    Se escribió el script para enviar correos desde php

--- testMail.php
+++ testMail.php
@@ -1,11 +1,11 @@
 <?php
-    ini_set( 'display_errors', 1 );
+    ini_set( 'display_errors', 1 );//La línea 1 y 2 permiten informar si el script no se ejecuta
     error_reporting( E_ALL );
-    $from = "test@hostinger-tutorials.com";
-    $to = "test@hostinger.com";
-    $subject = "Checking PHP mail";
-    $message = "PHP mail works just fine";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    echo "The email message was sent.";
+    $from = "miguel@gestordeudas.online";//Correo desde el cual se va a enviar el mensaje
+    $to = "miguel.salazar865@pascualbravo.edu.co";//Destinatario del correo
+    $subject = "Probando el envío de correos desde PHP";//Asunto del correo
+    $message = "Parece que el envío desde PHP funciona";//Se redacta el mensaje
+    $headers = "From:" . $from;//Detalla la información vital, como la dirección del remitente, la ubicación de respuesta, etc.
+    mail($to,$subject,$message, $headers);//Esta línea ejecuta la función 
+    echo "El correo fue enviado con exito";
 ?>
