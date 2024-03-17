c9eb205c00e6f591198932c760802395c51c7c31
Miguel Anjel Salazar Jaramillo <miguesalas2004@gmail.com>
2024-03-17 13:50:07 -0500

    se cambión el titulo y se agregó la metadescripción

--- index.php
+++ index.php
@@ -33,7 +33,8 @@ if(isset($_POST['ver_abonos'])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <meta name="description" content="Página web GRATUITA que funciona como un gestor de deudas online">
+    <title>Gestor de deudas online</title>
 </head>
 <body>
     <head>
